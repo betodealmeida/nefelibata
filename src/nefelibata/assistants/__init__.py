9cfed32c692b4463e6fd095f4aad7d53f0d5b629
Beto Dealmeida <roberto@dealmeida.net>
2020-05-25 15:55:24 -0700

    Playlist assistant

---
+++ src/nefelibata/assistants/__init__.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from typing import Any, Dict, List, Optional
+from pathlib import Path
+
+from pkg_resources import iter_entry_points
+
+from nefelibata.post import Post
+
+
+class Scope(Enum):
+    POST = "post"
+    SITE = "site"
+
+
+class Assistant:
+
+    scope: List[Scope] = []
+
+    def process_post(self, post: Post) -> None:
+        if Scope.POST not in self.scope:
+            raise Exception(f'Scope "post" not supported by {self.__class__.__name__}')
+
+        raise NotImplementedError("Subclasses MUST implement `process_post`")
+
+    def process_site(self, root: Path) -> None:
+        if Scope.SITE not in self.scope:
+            raise Exception(f'Scope "site" not supported by {self.__class__.__name__}')
+
+        raise NotImplementedError("Subclasses MUST implement `process_site`")
+
+
+def get_assistants(
+    config: Dict[str, Any], scope: Optional[Scope] = None
+) -> List[Assistant]:
+    names = config["assistants"] or []
+
+    assistants = {a.name: a.load() for a in iter_entry_points("nefelibata.assistant")}
+    return [assistants[name]() for name in names]
---
+++ src/nefelibata/assistants/playlist.py
@@ -0,0 +1,58 @@
+import urllib.parse
+from pathlib import Path
+from typing import TypedDict
+
+from mutagen.mp3 import MP3
+
+from nefelibata.assistants import Assistant, Scope
+from nefelibata.post import Post
+from nefelibata.utils import get_config
+
+
+class MP3Info(TypedDict):
+    path: Path
+    title: str
+    artist: str
+    album: str
+    year: int
+    duration: int
+
+
+class PlaylistAssistant(Assistant):
+
+    scope = [Scope.POST]
+
+    def process_post(self, post: Post) -> None:
+        post_directory = post.file_path.parent
+
+        mp3s: List[MP3Info] = []
+        for path in post_directory.glob("**/*.mp3"):
+            filename = path.relative_to(post_directory)
+            mp3 = MP3(path)
+            mp3s.append(
+                {
+                    "path": path,
+                    "title": mp3.get("TIT2", filename.stem),
+                    "artist": mp3.get("TPE1", "Unknown artist"),
+                    "album": mp3.get("TALB", "Unknown album"),
+                    "year": mp3.get("TDRC", "Unknown year"),
+                    "duration": int(mp3.info.length),
+                }
+            )
+
+        if not mp3s:
+            return
+
+        lines = ["[playlist]", "", f"NumberOfEntries={len(mp3s)}", "Version=2", ""]
+        for i, mp3 in enumerate(mp3s):
+            relative_url = mp3["path"].relative_to(post.root / "posts")
+            url = urllib.parse.urljoin(post.config["url"], str(relative_url))
+            title = "{album} ({year}) - {artist} - {title}".format(**mp3)
+            lines.append(f"File{i+1}={url}")
+            lines.append(f"Title{i+1}={title}")
+            lines.append(f'Length{i+1}={mp3["duration"]}')
+            lines.append("")
+        pls = "\n".join(lines).strip()
+
+        with open(post_directory / "index.pls", "w") as fp:
+            fp.write(pls)
