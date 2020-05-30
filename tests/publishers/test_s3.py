f1a4f335bb06829a4d7ec34b75ff52eda940c510
Beto Dealmeida <roberto@dealmeida.net>
2020-05-29 19:41:02 -0700

    S3 unit tests

--- tests/publishers/test_s3.py
+++ tests/publishers/test_s3.py
@@ -1,6 +1,11 @@
+import os
+from datetime import datetime
+from datetime import timezone
+from pathlib import Path
 from unittest import mock
 
 from botocore.exceptions import ClientError
+from freezegun import freeze_time
 from nefelibata.publishers.s3 import S3Publisher
 
 __author__ = "Beto Dealmeida"
@@ -179,3 +184,146 @@ def test_configure_route53_no_zone():
         publisher._configure_route53("blog.example.com.")
 
     mock_boto3_client.return_value.change_resource_record_sets.assert_not_called()
+
+
+def test_publish(fs):
+    publisher = S3Publisher(
+        bucket="blog.example.com",
+        AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID",
+        AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY",
+        configure_website=True,
+        configure_route53="blog.example.com.",
+        region="us-east-1",
+    )
+
+    root = Path("/path/to/blog")
+    fs.create_dir(root)
+    with freeze_time("2020-01-01T00:00:00Z"):
+        fs.create_file(root / "last_published")
+
+    # create 2 posts, 1 of them up-to-date
+    fs.create_dir(root / "build")
+    with freeze_time("2019-12-31T00:00:00Z"):
+        fs.create_dir(root / "build/one")
+        fs.create_file(root / "build/one/index.html")
+    with freeze_time("2020-01-02T00:00:00Z"):
+        fs.create_dir(root / "build/two")
+        fs.create_file(root / "build/two/index.html")
+
+    with freeze_time("2020-01-03T00:00:00Z"):
+        with mock.patch("nefelibata.publishers.s3.boto3.client") as mock_boto3_client:
+            mock_boto3_client.return_value.list_hosted_zones.return_value = {
+                "HostedZones": [
+                    {"Name": "foobar.com", "Id": "2"},
+                    {"Name": "example.com.", "Id": "1"},
+                ],
+            }
+            publisher.publish(root)
+
+    mock_boto3_client.return_value.upload_file.assert_called_once_with(
+        "/path/to/blog/build/two/index.html",
+        "blog.example.com",
+        "two/index.html",
+        ExtraArgs={"ACL": "public-read", "ContentType": "text/html"},
+    )
+    mtime = (root / "last_published").stat().st_mtime
+    assert (
+        datetime.fromtimestamp(mtime).astimezone(timezone.utc).isoformat()
+        == "2020-01-03T00:00:00+00:00"
+    )
+
+
+def test_publish_no_last_published(fs):
+    publisher = S3Publisher(
+        bucket="blog.example.com",
+        AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID",
+        AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY",
+        region="us-east-1",
+    )
+
+    root = Path("/path/to/blog")
+    fs.create_dir(root)
+
+    # create 2 posts, 1 of them up-to-date
+    fs.create_dir(root / "build")
+    with freeze_time("2019-12-31T00:00:00Z"):
+        fs.create_dir(root / "build/one")
+        fs.create_file(root / "build/one/index.html")
+    with freeze_time("2020-01-02T00:00:00Z"):
+        fs.create_dir(root / "build/two")
+        fs.create_file(root / "build/two/index.html")
+
+    with freeze_time("2020-01-03T00:00:00Z"):
+        with mock.patch("nefelibata.publishers.s3.boto3.client") as mock_boto3_client:
+            publisher.publish(root)
+
+    mock_boto3_client.return_value.upload_file.assert_has_calls(
+        [
+            mock.call(
+                "/path/to/blog/build/two/index.html",
+                "blog.example.com",
+                "two/index.html",
+                ExtraArgs={"ACL": "public-read", "ContentType": "text/html"},
+            ),
+            mock.call(
+                "/path/to/blog/build/one/index.html",
+                "blog.example.com",
+                "one/index.html",
+                ExtraArgs={"ACL": "public-read", "ContentType": "text/html"},
+            ),
+        ],
+    )
+    mtime = (root / "last_published").stat().st_mtime
+    assert (
+        datetime.fromtimestamp(mtime).astimezone(timezone.utc).isoformat()
+        == "2020-01-03T00:00:00+00:00"
+    )
+
+
+def test_publish_broken_symlink(fs):
+    publisher = S3Publisher(
+        bucket="blog.example.com",
+        AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID",
+        AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY",
+        region="us-east-1",
+    )
+
+    root = Path("/path/to/blog")
+    fs.create_dir(root)
+
+    # create 2 posts, 1 of them up-to-date
+    fs.create_dir(root / "build")
+    with freeze_time("2019-12-31T00:00:00Z"):
+        fs.create_dir(root / "build/one")
+        fs.create_file(root / "build/one/index.html")
+    with freeze_time("2020-01-02T00:00:00Z"):
+        fs.create_dir(root / "build/two")
+        fs.create_file(root / "build/two/index.html")
+
+    os.symlink(root / "posts/non-existent", root / "build/three")
+
+    with freeze_time("2020-01-03T00:00:00Z"):
+        with mock.patch("nefelibata.publishers.s3.boto3.client") as mock_boto3_client:
+            publisher.publish(root)
+
+    mock_boto3_client.return_value.upload_file.assert_has_calls(
+        [
+            mock.call(
+                "/path/to/blog/build/two/index.html",
+                "blog.example.com",
+                "two/index.html",
+                ExtraArgs={"ACL": "public-read", "ContentType": "text/html"},
+            ),
+            mock.call(
+                "/path/to/blog/build/one/index.html",
+                "blog.example.com",
+                "one/index.html",
+                ExtraArgs={"ACL": "public-read", "ContentType": "text/html"},
+            ),
+        ],
+    )
+    mtime = (root / "last_published").stat().st_mtime
+    assert (
+        datetime.fromtimestamp(mtime).astimezone(timezone.utc).isoformat()
+        == "2020-01-03T00:00:00+00:00"
+    )
