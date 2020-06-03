777773a75666e85a1d4809a8534a2b87d02e41b4
Beto Dealmeida <roberto@dealmeida.net>
2020-06-02 21:43:37 -0700

    Adding unit tests to Neocities

---
+++ src/nefelibata/publishers/neocities.py
@@ -0,0 +1,80 @@
+import logging
+import mimetypes
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+
+import requests
+from nefelibata.publishers import Publisher
+
+_logger = logging.getLogger(__name__)
+
+
+class NeocitiesPublisher(Publisher):
+
+    """A publisher that uploads the weblog to Neocities."""
+
+    def __init__(
+        self,
+        root: Path,
+        config: Dict[str, Any],
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        api_key: Optional[str] = None,
+    ):
+        super().__init__(root, config)
+
+        if (username and password) == (api_key is not None):
+            raise Exception(
+                "You must provide exactly ONE of username/password or API key",
+            )
+
+        if username and password:
+            response = requests.get(
+                f"https://{username}:{password}@neocities.org/api/key",
+            )
+            payload = response.json()
+            api_key = payload["api_key"]
+            _logger.warning(
+                f"Your API key is {api_key}, please remove the username/password from the configuration and add the API key",
+            )
+
+        self.headers = {"Authorization": f"Bearer {api_key}"}
+
+    def publish(self, force: bool = False) -> None:
+        # store file with the last time weblog was published
+        last_published_file = self.root / "last_published"
+        if last_published_file.exists():
+            last_published = last_published_file.stat().st_mtime
+        else:
+            last_published = 0
+
+        build = self.root / "build"
+        queue = [build]
+        # manually walk, since `glob("**/*")` doesn't follow symlinks
+        filenames: List[Tuple[Path, str]] = []
+        while queue:
+            current = queue.pop()
+            for path in current.glob("*"):
+                if not path.exists():
+                    # broken symlink
+                    continue
+                if path.is_dir():
+                    queue.append(path)
+                elif force or path.stat().st_mtime > last_published:
+                    key = str(path.relative_to(build))
+                    filenames.append((path, key))
+
+        # NeoCities API expects a dict in the following format:
+        # { name_on_server: <file_object> }
+        args = {pair[1]: open(pair[0], "rb") for pair in filenames}
+        print(args)
+
+        url = "https://neocities.org/api/upload"
+        requests.post(url, files=args, headers=self.headers)
+
+        # update last published
+        last_published_file.touch()
