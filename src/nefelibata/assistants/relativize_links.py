1c205a9f388a925d5041a20566fea9905651a588
Beto Dealmeida <roberto@dealmeida.net>
2020-06-13 15:07:26 -0700

    Assistant to make links relative

---
+++ src/nefelibata/assistants/relativize_links.py
@@ -0,0 +1,77 @@
+import logging
+import urllib.parse
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from nefelibata.assistants import Assistant
+from nefelibata.assistants import Scope
+from nefelibata.post import Post
+
+_logger = logging.getLogger(__name__)
+
+
+class RelativizeLinksAssistant(Assistant):
+
+    scopes = [Scope.POST, Scope.SITE]
+
+    def process_post(self, post: Post, force: bool = False) -> None:
+        self._process_file(post.file_path.with_suffix(".html"))
+
+    def process_site(self, force: bool = False) -> None:
+        for path in (self.root / "build").glob("*.html"):
+            self._process_file(path)
+
+    def _process_file(self, file_path: Path) -> None:
+        with open(file_path) as fp:
+            html = fp.read()
+
+        tag_attributes = [
+            ("a", "href"),
+            ("img", "src"),
+            ("link", "href"),
+            ("script", "src"),
+        ]
+        soup = BeautifulSoup(html, "html.parser")
+        modified = False
+        for tag, attr in tag_attributes:
+            for el in soup.find_all(tag):
+                resource = el.attrs.get(attr)
+                if not resource:
+                    continue
+
+                # this should be "posts", when `process_post` is called, or "build",
+                # when `process_site` is called
+                base_dir = file_path.relative_to(self.root).parts[0]
+
+                if resource.startswith(self.config["url"]):
+                    print(1)
+                    resource_path = (
+                        self.root / base_dir / resource[len(self.config["url"]) :]
+                    )
+                elif resource.startswith("/"):
+                    print(2)
+                    resource_path = self.root / base_dir / resource[1:]
+                else:
+                    print(3)
+                    # relative already
+                    continue
+
+                up = Path(".")
+                parent = file_path.parent
+                while True:
+                    try:
+                        # we're guaranteed to reach this eventually
+                        relative_url = resource_path.relative_to(parent)
+                        break
+                    except ValueError:
+                        # resource is up of file path
+                        parent = parent.parent
+                        up = up / ".."
+
+                el.attrs[attr] = up / relative_url
+                modified = True
+
+        if modified:
+            html = str(soup)
+            with open(file_path, "w") as fp:
+                fp.write(html)
---
+++ tests/assistants/test_relativize_links.py
@@ -0,0 +1,124 @@
+from datetime import datetime
+from datetime import timezone
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from unittest import mock
+
+from freezegun import freeze_time
+from nefelibata.assistants.relativize_links import RelativizeLinksAssistant
+from nefelibata.builders.post import PostBuilder
+
+__author__ = "Beto Dealmeida"
+__copyright__ = "Beto Dealmeida"
+__license__ = "mit"
+
+
+config: Dict[str, Any] = {
+    "url": "https://example.com/",
+    "language": "en",
+    "theme": "test-theme",
+    "webmention": {"endpoint": "https://webmention.io/example.com/webmention"},
+}
+
+
+def test_process_post(mock_post):
+    root = Path("/path/to/blog")
+
+    with freeze_time("2020-01-01T00:00:00Z"):
+        post = mock_post(
+            """
+        subject: Hello, World!
+        keywords: test
+        summary: My first post
+
+        Hi, there!
+
+        <a id="4-links">Four links</a>:
+
+        - [Atom feed](https://example.com/atom.xml)
+        - [An image](img/sample.jpg)
+        - [My favorite mp3](file.mp3)
+        - [My favorite CSS](/css/style.css)
+        """,
+        )
+    PostBuilder(root, config).process_post(post)
+
+    assistant = RelativizeLinksAssistant(root, config)
+    assistant.process_post(post)
+
+    with open(post.file_path.with_suffix(".html")) as fp:
+        html = fp.read()
+
+    assert (
+        html
+        == """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://webmention.io/example.com/webmention" rel="webmention"/>
+<link href="https://external.example.com/css/basic.css" rel="stylesheet"/>
+<link href="../css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<p>Hi, there!</p>
+<p><a id="4-links">Four links</a>:</p>
+<ul>
+<li><a href="../atom.xml">Atom feed</a></li>
+<li><a href="img/sample.jpg">An image</a></li>
+<li><a href="file.mp3">My favorite mp3</a></li>
+<li><a href="../css/style.css">My favorite CSS</a></li>
+</ul>
+</body>
+</html>"""
+    )
+
+
+def test_process_post_no_modification(mock_post):
+    root = Path("/path/to/blog")
+
+    with freeze_time("2020-01-01T00:00:00Z"):
+        post = mock_post(
+            """
+        subject: Hello, World!
+        keywords: test
+        summary: My first post
+
+        Hi, there!
+
+        <a id="4-links">Four links</a>:
+
+        - [Atom feed](https://example.com/atom.xml)
+        - [An image](img/sample.jpg)
+        - [My favorite mp3](file.mp3)
+        - [My favorite CSS](/css/style.css)
+        """,
+        )
+        PostBuilder(root, config).process_post(post)
+
+    assistant = RelativizeLinksAssistant(root, config)
+
+    with freeze_time("2020-01-02T00:00:00Z"):
+        assistant.process_post(post)
+    with freeze_time("2020-01-03T00:00:00Z"):
+        assistant.process_post(post)
+
+    assert datetime.fromtimestamp(
+        post.file_path.with_suffix(".html").stat().st_mtime,
+    ).astimezone(timezone.utc) == datetime(2020, 1, 2, 0, 0, tzinfo=timezone.utc)
+
+
+def test_process_site(fs):
+    root = Path("/path/to/blog")
+
+    fs.create_dir(root / "build")
+    with open(root / "build/test.html", "w") as fp:
+        fp.write('<img src="/img/background.jpg" />')
+
+    assistant = RelativizeLinksAssistant(root, config)
+    assistant.process_site()
+
+    with open(root / "build/test.html") as fp:
+        html = fp.read()
+
+    assert html == '<img src="img/background.jpg"/>'
