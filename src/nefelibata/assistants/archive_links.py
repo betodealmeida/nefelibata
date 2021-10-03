7cb226157538230655efc366df4d120338d0fb13
Beto Dealmeida <roberto@dealmeida.net>
2021-10-03 11:10:13 -0700

    Archive links assistant

---
+++ src/nefelibata/assistants/archive_links.py
@@ -0,0 +1,63 @@
+"""
+Assistant for saving external links in https://archive.org/.
+"""
+
+import asyncio
+import logging
+from datetime import timedelta
+from typing import Any, Dict, Iterator
+
+import marko
+from aiohttp import ClientSession
+
+from nefelibata.announcers.base import Scope
+from nefelibata.assistants.base import Assistant
+from nefelibata.post import Post
+
+_logger = logging.getLogger(__name__)
+
+
+# API is restricted to 5 requests per minute, see
+# https://rationalwiki.org/wiki/Internet_Archive#Restrictions
+lock = asyncio.Lock()
+SLEEP = timedelta(seconds=12)
+
+
+def extract_links(content: str) -> Iterator[str]:
+    """
+    Extract all links from a Markdown document.
+    """
+    tree = marko.parse(content)
+    queue = [tree]
+    while queue:
+        element = queue.pop()
+
+        if isinstance(element, marko.inline.Link):
+            yield element.dest
+        elif hasattr(element, "children"):
+            queue.extend(element.children)
+
+
+class ArchiveLinksAssistant(Assistant):
+    """
+    Assistant for saving external links in https://archive.org/.
+    """
+
+    name = "saved_links"
+    scopes = [Scope.POST]
+
+    async def get_post_metadata(self, post: Post) -> Dict[str, Any]:
+        saved_links = {}
+
+        async with ClientSession() as session:
+            for uri in extract_links(post.content):
+                _logger.info("Saving URI %s", uri)
+                save_uri = f"https://web.archive.org/save/{uri}"
+                async with lock:
+                    async with session.get(save_uri) as response:
+                        for rel, params in response.links.items():
+                            if rel == "memento":
+                                saved_links[uri] = str(params["url"])
+                    await asyncio.sleep(SLEEP.total_seconds())
+
+        return saved_links
---
+++ tests/assistants/test_archive_links.py
@@ -0,0 +1,103 @@
+"""
+Tests for ``nefelibata.assistants.archive_links``.
+"""
+
+from datetime import timedelta
+from pathlib import Path
+
+import pytest
+from pytest_mock import MockerFixture
+from yarl import URL
+
+from nefelibata.assistants.archive_links import ArchiveLinksAssistant, extract_links
+from nefelibata.config import Config
+from nefelibata.post import Post
+
+
+@pytest.mark.asyncio
+async def test_assistant(
+    mocker: MockerFixture,
+    root: Path,
+    config: Config,
+    post: Post,
+) -> None:
+    """
+    Test ``get_memento``.
+    """
+    # disable sleep
+    mocker.patch("nefelibata.assistants.archive_links.SLEEP", timedelta(seconds=0))
+
+    links = {
+        "original": {
+            "rel": "original",
+            "url": URL("https://nefelibata.readthedocs.io/"),
+        },
+        "timemap": {
+            "rel": "timemap",
+            "type": "application/link-format",
+            "url": URL(
+                "https://web.archive.org/web/timemap/link/https://nefelibata.readthedocs.io/",
+            ),
+        },
+        "timegate": {
+            "rel": "timegate",
+            "url": URL(
+                "https://web.archive.org/web/https://nefelibata.readthedocs.io/",
+            ),
+        },
+        "first memento": {
+            "rel": "first memento",
+            "datetime": "Sun, 05 Aug 2018 06:22:28 GMT",
+            "url": URL(
+                "https://web.archive.org/web/20180805062228/http://nefelibata.readthedocs.io/",
+            ),
+        },
+        "prev memento": {
+            "rel": "prev memento",
+            "datetime": "Tue, 29 Jun 2021 01:15:06 GMT",
+            "url": URL(
+                "https://web.archive.org/web/20210629011506/https://nefelibata.readthedocs.io/",
+            ),
+        },
+        "memento": {
+            "rel": "memento",
+            "datetime": "Sun, 03 Oct 2021 15:46:02 GMT",
+            "url": URL(
+                "https://web.archive.org/web/20211003154602/https://nefelibata.readthedocs.io/",
+            ),
+        },
+        "last memento": {
+            "rel": "last memento",
+            "datetime": "Sun, 03 Oct 2021 15:46:02 GMT",
+            "url": URL(
+                "https://web.archive.org/web/20211003154602/https://nefelibata.readthedocs.io/",
+            ),
+        },
+    }
+    get = mocker.patch("nefelibata.assistants.archive_links.ClientSession.get")
+    get.return_value.__aenter__.return_value.links = links
+
+    assistant = ArchiveLinksAssistant(root, config)
+
+    response = await assistant.get_post_metadata(post)
+    assert response == {
+        "https://nefelibata.readthedocs.io/": (
+            "https://web.archive.org/web/20211003154602/"
+            "https://nefelibata.readthedocs.io/"
+        ),
+    }
+
+
+def test_extract_links() -> None:
+    """
+    Test ``extract_links``.
+    """
+
+    assert list(extract_links("No links here, move along")) == []
+
+    content = """
+This is a long document with [links](https://foo.example.com/).
+
+And another: https://bar.example.com/
+    """
+    assert list(extract_links(content)) == ["https://foo.example.com/"]
